6563a4ca1f09ba1136ccf602b50ef95f20889708
Peter <peter@waytofly.com>
2022-12-23 13:15:58 +0100

    initial

---
+++ src/main.js
@@ -0,0 +1,39 @@
+import inquirer from 'inquirer';
+import { io } from "socket.io-client";
+
+const CLI = async () => {
+    const socket = io("ws://localhost:3000");
+    socket.on('connect', () => {
+        console.log('-- client connected');
+        inquirer.prompt([
+            {
+            type: 'input',
+            name: 'name',
+            message: 'Which character would you like to search for?'
+            }
+        ]).then(async answers => {
+            try {
+                console.log(`Searching for ${answers.name}...`);
+                await new Promise((resolve, reject) => {
+                    socket.emit('search', { 'query': answers.name });
+                    socket.on('search', (res) => {
+                        const { resultCount, page, name, films } = res;
+                        console.log(`(${page}/${resultCount}) ${name} - [${films}]`);
+                        if (page === resultCount ) {
+                            resolve(true);
+                        }
+                    });
+                    socket.on('error', () => {
+                        reject(`ERR: No valid matches found for query ${answers.name}`);
+                    });
+                });
+                CLI();
+            } catch (error) {
+                console.log('Error: ', error);
+                CLI();
+            }
+        });
+    });
+}
+
+CLI();
